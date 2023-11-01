8fe825d77e17cd927f1af191c60dfc90ed9a9718
Adriano Grevizirsky <grevizirsky@gmail.com>
2023-10-31 22:12:07 -0300

    Adicionando components

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,29 @@
 
 import './App.css'
 
+import About from './components/About'
+import Contact from './components/Contact'
+import Footer from './components/Footer'
+import Hero from './components/Hero'
+import Projects from './components/Projects'
+import Services from './components/Services'
+import Skills from './components/Skills'
+
 function App() {
   return (
     <>
-      <h1 className='text-4xl'>app</h1>
+      <main>
+        <Hero />
+        <About />
+        <Services />
+        <Projects />
+        <Skills />
+        <Contact />
+      </main>
+      <footer>
+        <Footer />
+      </footer>
+      <p>Desenvolvedor Front-End Especializado em React: Criando Interfaces de Usuário Dinâmicas e Responsivas para a Web do Futuro</p>
     </>
   )
 }

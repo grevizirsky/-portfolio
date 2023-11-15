60701bd87b235231a2f3d94b85c4621551ec14e6
Adriano Grevizirsky <grevizirsky@gmail.com>
2023-11-15 16:41:45 -0300

    Componente Footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,20 @@
-import React from "react";
+import React from "react"
 
-export default function Footer(){
+export default function Footer() {
     return (
-        <h1>Footer</h1>
+      <div className="container mx-auto flex max-w-4xl p-4 text-sm">
+        <p className="flex-grow">
+          &copy; {new Date().getFullYear()} &middot; Todos os direitos reservados.
+        </p>
+        <ul className="flex flex-nowrap gap-2">
+          <li>
+            <a href="#">Termos de uso</a>
+          </li>
+          <li className="text-gray-500">&middot;</li>
+          <li>
+            <a href="#">Política de privacidade</a>
+          </li>
+        </ul>
+      </div>
     )
-}
+  }

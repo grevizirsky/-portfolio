3dec3504958e05bec340f218a62637249dfe5515
Adriano Grevizirsky <grevizirsky@gmail.com>
2023-11-15 17:33:00 -0300

    Adicionando download de curriculo na Hero

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -38,9 +38,10 @@ export default function Hero() {
               </a>
               <span className="italic text-gray-500">ou</span>
               <a
-                href="#"
+                href="./src/assets/Currículo Adriano Grevizirsky.pdf"
                 target="_blank"
                 className="button flex items-center gap-2 text-gray-600 hover:text-gray-400"
+                download
               >
                 <HiDownload />
                 Baixe meu CV
